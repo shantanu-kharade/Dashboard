2ccf212612f6a57abe325e7cc31e750dae50f79a
shantanu-kharade <shantanu.kharade27@gmail.com>
2025-11-03 14:50:55 +0530

    updated the Inventory page

--- src/components/InventoryCard.jsx
+++ src/components/InventoryCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InventoryCard = ({ device, model, price, quantity }) => {
     return (
-        <div className="bg-white-100 text-black border-1 border-indigo-600  rounded-md w-88 h-48 p-6 flex items-center justify-between hover:shadow-2xl">
+        <div className="bg-white text-black border-1 border-indigo-600  rounded-md w-88 h-48 p-6 flex items-center justify-between hover:shadow-2xl">
             {/* Text Section */}
             <div className="flex flex-col justify-center space-y-1">
                 <h2 className="text-xl font-semibold">{device}</h2>
